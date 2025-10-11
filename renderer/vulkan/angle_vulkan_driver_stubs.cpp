7de53ec48b7b17be3b6a5f7ab17176631af62335
HorizO0n <162360926+HorizO0n@users.noreply.github.com>
2025-10-10 21:56:26 -0300

    Create angle_vulkan_driver_stubs.cpp

---
+++ renderer/vulkan/angle_vulkan_driver_stubs.cpp
@@ -0,0 +1,13 @@
+
+#include "angle_vulkan_driver_stubs.h"
+
+namespace angle {
+
+// Stubs Vulkan System Info
+unsigned int ParseArmVulkanDriverVersion(unsigned int) { return 0; }
+unsigned int ParseQualcommVulkanDriverVersion(unsigned int) { return 0; }
+unsigned int ParseNvidiaVulkanDriverVersion(unsigned int) { return 0; }
+unsigned int ParseSamsungVulkanDriverVersion(unsigned int) { return 0; }
+unsigned int ParseAMDVulkanDriverVersion(unsigned int) { return 0; }
+
+}  // namespace angle
